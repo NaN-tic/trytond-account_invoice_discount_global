15a600728575f8992c100b090f35d45cee545463
Guillem Barba <guillem@nan-tic.com>
2016-04-21 09:17:35 +0200

    Remove invisible states for supplier invoices

--- invoice.py
+++ invoice.py
@@ -24,7 +24,6 @@ class Invoice:
     invoice_discount = fields.Numeric('Invoice Discount',
         digits=(16, DISCOUNT_DIGITS), states={
             'readonly': Eval('state') != 'draft',
-            'invisible': Eval('type').in_(['in_invoice', 'in_credit_note']),
             }, depends=['state'])
 
     @classmethod
@@ -104,7 +103,7 @@ class Invoice:
                     lines.remove(line)
                     to_delete.append(line)
             invoice.lines = lines
-        
+
         Line.delete(to_delete)
         for invoice in invoices:
             invoice.save()
