cf7ed98fc99889272bb9a5d5bd0b6c27a11da37e
Jes?s Mart?n Jim?nez <jesus@nan-tic.com>
2016-07-21 12:54:59 +0200

    Update test to version 4.0

--- tests/test_account_invoice_discount_global.py
+++ tests/test_account_invoice_discount_global.py
@@ -4,24 +4,14 @@ import doctest
 import unittest
 
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import doctest_checker
 
 
-class TestCase(unittest.TestCase):
+class TestCase(ModuleTestCase):
     'Test account_invoice_discount_global module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module(
-            'account_invoice_discount_global')
-
-    def test0005views(self):
-        'Test views'
-        test_view('account_invoice_discount_global')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'account_invoice_discount_global'
 
 
 def suite():
@@ -29,8 +19,10 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCase))
     suite.addTests(doctest.DocFileSuite('scenario_invoice.rst',
             setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            checker=doctest_checker,
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     suite.addTests(doctest.DocFileSuite('scenario_invoice_sale_purchase.rst',
             setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            checker=doctest_checker,
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
