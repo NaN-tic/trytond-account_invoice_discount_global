363df635eaf1e83fb7319842e3f380c51dc5333e
?ngel ?lvarez <angel@nan-tic.com>
2018-06-28 17:06:23 +0200

    fix tests and extras_depend

--- __init__.py
+++ __init__.py
@@ -12,6 +12,12 @@ def register():
         invoice.Configuration,
         invoice.Invoice,
         invoice.InvoiceLine,
+        module='account_invoice_discount_global', type_='model')
+    Pool.register(
         invoice.Purchase,
+        depends=['purchase'],
+        module='account_invoice_discount_global', type_='model')
+    Pool.register(
         invoice.Sale,
+        depends=['sale'],
         module='account_invoice_discount_global', type_='model')
