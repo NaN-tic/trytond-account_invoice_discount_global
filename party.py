4b7cd86fcf11ae48118cab273c89ec75f7a14a3c
?ngel ?lvarez <angel@nan-tic.com>
2019-06-20 12:55:13 +0200

    add migration from version 3.4

--- party.py
+++ party.py
@@ -4,6 +4,10 @@ from trytond.config import config
 from trytond.model import fields
 from trytond.pool import Pool, PoolMeta
 from trytond.pyson import Eval
+from trytond.transaction import Transaction
+from sql import Null
+from trytond import backend
+
 
 __all__ = ['Party', 'PartyAccount']
 
@@ -24,6 +28,26 @@ class Party(metaclass=PoolMeta):
                 }))
 
     @classmethod
+    def __register__(cls, module_name):
+        Property = pool.get('ir.property')
+        TableHandler = backend.get('TableHandler')
+
+        super(Party, cls).__register__(module_name)
+        cursor = Transaction().connection.cursor()
+        handler = TableHandler(cls, module_name)
+        table = cls.__table__()
+        # Migration from 3.4.0: moved invoice_discount to property
+        # customer_invoice_discount
+        if handler.column_exist('invoice_discount'):
+            cursor.execute(*table.select(table.id, table.invoice_discount,
+                    where=table.invoice_discount != Null))
+            for party_id, invoice_discount in cursor.fetchall():
+                Property.set('customer_invoice_discount', cls.__name__,
+                    [party_id], ',%s' % invoice_discount.to_eng_string())
+            handler.drop_column('invoice_discount', exception=True)
+
+
+    @classmethod
     def multivalue_model(cls, field):
         pool = Pool()
         if field in {'customer_invoice_discount', 'supplier_invoice_discount'}:
@@ -38,3 +62,12 @@ class PartyAccount(metaclass=PoolMeta):
         "Customer Invoice Discount", digits=DISCOUNT_DIGITS)
     supplier_invoice_discount = fields.Numeric(
         "Supplier Invoice Discount", digits=DISCOUNT_DIGITS)
+
+    @classmethod
+    def _migrate_property(cls, field_names, value_names, fields):
+        field_names += ['customer_invoice_discount',
+            'supplier_invoice_discount']
+        value_names += ['customer_invoice_discount',
+            'supplier_invoice_discount']
+        super(PartyAccount, cls)._migrate_property(field_names, value_names,
+            fields)
